fff97f7bf7a667a429d7705bf462b7c19f2b2378
Vitória Stefany <vitoriaste@gmail.com>
2023-11-11 15:43:29 -0300

    Creating the register route

--- app.js
+++ app.js
@@ -7,9 +7,24 @@ const jwt = require('jsonwebtoken')
 
 const app = express()
 
+// Config JSON response
+app.use(express.json)
+
 // Open Route - Public Route
 app.get('/', (req, res) => {
     res.status(200).json({ msg: "Bem vindo a nossa API!" })
+})
+
+// Register User
+app.post('/auth/register', async(req, res) => {
+
+    const {name, email, password, confirmPassword} = req.body
+
+    // validations
+    if(!name){
+        return res.status(422).json({msg: "O nome é obrigatório! "}) // Requisão entendida pelo servidor, porém os dados estão incorretos!
+    }
+
 })
 
 // Credencials
