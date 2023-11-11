6b46787645f23c7b48370866ea2c8732c7518378
Vitória Stefany <vitoriaste@gmail.com>
2023-11-11 15:31:30 -0300

    Connecting to the database

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 /* imports */
 require('dotenv').config()
 const express = require('express')
-const mongoose = requiser('mongoose')
+const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
 const app = express()
 
-app.listen(3000)
+// Open Route - Public Route
+app.get('/', (req, res) => {
+    res.status(200).json({ msg: "Bem vindo a nossa API!" })
+})
+
+// Credencials
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
+
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.5lmk5ho.mongodb.net/?retryWrites=true&w=majority`)
+.then(() => {
+    app.listen(3030)
+    console.log("Conectado ao banco!")
+})
+.catch((err) => consolelog(err))
